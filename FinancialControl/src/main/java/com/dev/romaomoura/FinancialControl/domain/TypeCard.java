3343cc869fa4b784dcccff9ffe50d3306377e855
Romão Moura <c.romaomoura@gmail.com>
2024-11-14 18:06:09 -0300

    Add Docs, resourcer

--- FinancialControl/src/main/java/com/dev/romaomoura/FinancialControl/domain/TypeCard.java
+++
@@ -1,9 +0,0 @@
-package com.dev.romaomoura.FinancialControl.domain;
-
-public enum TypeCard {
-    CREDIT,
-    DEBIT,
-    DOUBLE_DEBIT_CREDIT,
-    FOOD_MEAL
-
-}
--- FinancialControl/src/main/java/com/dev/romaomoura/FinancialControl/repository/ExpenseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dev.romaomoura.FinancialControl.repository;
-
-import com.dev.romaomoura.FinancialControl.domain.Expense;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExpenseRepository extends JpaRepository<Expense, Long> {
-}
